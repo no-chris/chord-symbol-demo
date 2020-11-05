199a9fc290a725723154ff3e078f7dafaa8a4602
Christophe Noël <touffi51@gmail.com>
2020-11-05 15:49:26 +0100

    apply prettier rules

--- src/components/showcase/CanvasRenderer/CanvasComponent.js
+++ src/components/showcase/CanvasRenderer/CanvasComponent.js
@@ -6,42 +6,42 @@ import './CanvasRenderer.scss';
 export const CanvasComponent = (renderedChordRaw) => {
 	const canvasRef = useRef(null);
 
+	const font = '80px Petaluma Script';
+
 	useEffect(() => {
+		const draw = (ctx) => {
+			ctx.save();
+			ctx.font = font;
+			const formatter = new BbFormat(ctx);
+
+			const laidOutChord = formatter.layoutChordSymbol(
+				renderedChordRaw.input.symbol
+			);
+			if (!laidOutChord) return;
+
+			const margin = 10;
+
+			ctx.canvas.width = laidOutChord.bbox.width + margin * 2;
+			ctx.canvas.height = laidOutChord.bbox.height + margin * 2;
+
+			// After resizing the canvas, the context is reset
+			ctx.font = font;
+
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+			ctx.translate(margin, margin - laidOutChord.bbox.y);
+			formatter.fillText(
+				laidOutChord,
+				laidOutChord.bbox.x,
+				laidOutChord.bbox.y + laidOutChord.yOverflow
+			);
+			ctx.restore();
+		};
+
 		const canvas = canvasRef.current;
 		const context = canvas.getContext('2d');
 		draw(context);
 	}, [renderedChordRaw]);
-
-	const font = '80px Petaluma Script';
-
-	const draw = (ctx) => {
-		ctx.save();
-		ctx.font = font;
-		const formatter = new BbFormat(ctx);
-
-		const laidOutChord = formatter.layoutChordSymbol(
-			renderedChordRaw.input.symbol
-		);
-		if (!laidOutChord) return;
-
-		const margin = 10;
-
-		ctx.canvas.width = laidOutChord.bbox.width + margin * 2;
-		ctx.canvas.height = laidOutChord.bbox.height + margin * 2;
-
-		// After resizing the canvas, the context is reset
-		ctx.font = font;
-
-		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-		ctx.translate(margin, margin - laidOutChord.bbox.y);
-		formatter.fillText(
-			laidOutChord,
-			laidOutChord.bbox.x,
-			laidOutChord.bbox.y + laidOutChord.yOverflow
-		);
-		ctx.restore();
-	};
 
 	return <canvas ref={canvasRef} />;
 };
